141b70556f3851b5ea200804681ceef85eca4c0f
Adrian Dawid <adawid@localhost.localdomain>
2014-11-29 15:16:30 +0100

    * Added linux/os x setup

---
+++ setup.sh
@@ -0,0 +1,4 @@
+echo "export EMSCRIPTEN_BUILD_TOOL_HOME=$PWD" >> ~/.bash_profile
+echo "export PATH=\$PATH:\$EMSCRIPTEN_BUILD_TOOL_HOME" >> ~/.bash_profile
+source ~/.bash_profile
+echo "Please run source ~/.bash_profile"
